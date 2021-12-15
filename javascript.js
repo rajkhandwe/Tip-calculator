89b3341839e79a99f8d5224037ac345befd67f53
Raj <rajkhandwe@gmail.com>
2021-12-15 16:52:05 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function() {
+    let billAmount = document.getElementById('billAmount').value;
+    let tip = document.getElementById('tip').value;
+
+    let tipAmount = document.getElementById('tip-amount').value = billAmount / tip;
+    document.getElementById('total-bill').innerHTML = parseInt(billAmount) + parseFloat(tipAmount);
+    
+})
